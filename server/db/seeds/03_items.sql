433837e58c4d7deab4d6e954c1111a2712ccae00
tylerchessa <tylerchessa@gmail.com>
2023-04-13 17:35:41 +0000

    update schema to use TIMESTAMP

--- server/db/seeds/03_items.sql
+++ server/db/seeds/03_items.sql
@@ -2,5 +2,5 @@ INSERT INTO items (
   user_id, category_id, title, description, condition, end_date
 ) 
 VALUES (
-  4, 1, 'Original Iphone 1', 'Brand new, still in box. Signed by Steve Jobs', 5, '2023-04-28'
+  4, 1, 'Original Iphone 1', 'Brand new, still in box. Signed by Steve Jobs', 5, '2023-04-28 12:23:34.00'
 );
