70033d8df20b1b0da9f8d353fc002c0c45321216
Jenny Carroll <jennygc@telus.net>
2023-04-14 22:50:34 +0000

    seed item_images

--- server/db/seeds/04_item_images.sql
+++ server/db/seeds/04_item_images.sql
@@ -2,29 +2,55 @@ INSERT INTO item_images (
   item_id, img_url
 )
 VALUES (
-  1, 'https://imgur.com/5qo0n40'
+  1, 'https://imgur.com/5qo0n40.jpg'
 );
 INSERT INTO item_images (
   item_id, img_url
 )
 VALUES (
-  4, 'https://imgur.com/P8GssV7'
+  4, 'https://imgur.com/P8GssV7.jpg'
 );
 INSERT INTO item_images (
   item_id, img_url
 )
 VALUES (
-  3, 'https://imgur.com/LdLC1KB'
+  3, 'https://imgur.com/LdLC1KB.jpg'
 );
 INSERT INTO item_images (
   item_id, img_url
 )
 VALUES (
-  2, 'https://imgur.com/261EkzU'
+  2, 'https://imgur.com/261EkzU.jpg'
 );
 INSERT INTO item_images (
   item_id, img_url
 )
 VALUES (
-  5, 'https://imgur.com/E8YwXmh'
+  5, 'https://imgur.com/E8YwXmh.jpg'
 );
+
+INSERT INTO item_images (
+  item_id, img_url
+)
+VALUES (6, 'https://imgur.com/r5SZdtj.jpg'), 
+(6, 'https://imgur.com/XsBFQFu.jpg'),
+(6, 'https://imgur.com/u20seeF.jpg'),
+(6, 'https://i.imgur.com/K47MGgj.jpg'),
+(6, 'https://i.imgur.com/rj4r5Dv.jpg'),
+(7, 'https://i.imgur.com/w68f8Vu.jpg'),
+(7, 'https://i.imgur.com/9j9yQmZ.jpg'),
+(7, 'https://i.imgur.com/JBxib5p.jpg'),
+(7, 'https://i.imgur.com/SDy9xeE.jpg'),
+(7, 'https://i.imgur.com/EpDqthy.jpg'),
+(8, 'https://i.imgur.com/7aYJWyu.jpg'),
+(8, 'https://i.imgur.com/bOmG4CO.jpg'),
+(8, 'https://i.imgur.com/cNJIPz1.jpg'),
+(8, 'https://i.imgur.com/LPehhpD.jpg'),
+(9, 'https://i.imgur.com/h6Djcbi.jpg'),
+(9, 'https://i.imgur.com/t4OqfCS.jpg'),
+(9, 'https://i.imgur.com/ZXVq1M5.jpg'),
+(9, 'https://i.imgur.com/ebqYXxZ.jpg'),
+(10, 'https://i.imgur.com/PyxQ745.jpg'),
+(10, 'https://i.imgur.com/ynifBdj.jpg'),
+(10, 'https://i.imgur.com/HFmZEos.jpg'),
+(10, 'https://i.imgur.com/TBrsPvR.jpg')
