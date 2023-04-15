a545706a30a3d78dd41d1b27cfc9b1bc5dd3540e
Jenny Carroll <jennygc@telus.net>
2023-04-15 02:57:14 +0000

    create mock files for testing html and css

--- client/src/components/item.jsx
+++ client/src/components/item.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
+import "./item.scss"; //not sure if this is correct
 // import classNames from 'classnames';
 
 export default function Item(props) {
-  return <h1>This is an Item. {props.test}</h1>;
+  return <h1>This is where I will put my html. {props.test}</h1>;
 }
