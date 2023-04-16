11bfa3bb20942b8f2f028df5be813e964f637610
Jenny Carroll <jennygc@telus.net>
2023-04-16 13:33:01 -0600

    update itemDetail to useEffect and setState

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -1,19 +1,31 @@
+import { useState, useEffect } from "react";
 import "./ItemDetail.scss";
 import axios from "axios";
 
 import { useParams } from "react-router-dom";
 
-function ItemDetail(props) {
+function ItemDetail() {
   const params = useParams();
-  console.log("props.images***", props.images);
-  // images is an array of objects
 
-  axios.get(`/items/${params.itemId}`).then((res) => {
-    console.log("res", res);
-  });
+  const [itemObj, setItemObj] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`/items/${params.itemId}`)
+      .then((res) => {
+        setItemObj(res.data[0]);
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        console.log(error.response.data);
+      });
+  }, []);
 
   return (
-    <div className="itemDetail"> I am an Item Detail {params.itemId} </div>
+    <div className="itemDetail">
+      I am an {itemObj.description}Item Detail {params.itemId}
+    </div>
   );
 }
 
