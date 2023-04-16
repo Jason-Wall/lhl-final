25d695d4e4679fe58ceb29abee3fa113b8d924a6
Jason-Wall <jason.waldick@gmail.com>
2023-04-16 12:45:37 -0600

    Establish routing and component for new item

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Import components:
-import Items from "./Items";
-import Nav from "./Nav";
-import ItemDetail from "./ItemDetail";
-import Category from "./Category";
+import Items from './Items';
+import Nav from './Nav';
+import ItemDetail from './ItemDetail';
+import ItemNew from './ItemNew';
+import Category from './Category';
 
 // Import hooks and helpers:
-import useApplicationData from "../hooks/useApplicationData";
+import useApplicationData from '../hooks/useApplicationData';
 
 // Import styling:
-import "./App.scss";
+import './App.scss';
 
 // MAIN FUNCTION
 export default function App() {
@@ -22,13 +23,26 @@ export default function App() {
   return (
     <BrowserRouter>
       <main>
-        <Nav items={state.items} categories={state.categories} />
+        <Nav
+          items={state.items}
+          categories={state.categories}
+        />
 
         <Routes>
-          <Route path="/" element={<Items images={state.images} />}></Route>
-          <Route path="items/:itemId" element={<ItemDetail />}></Route>
           <Route
-            path="/categories/:categoryId"
+            path='/'
+            element={<Items images={state.images} />}
+          ></Route>
+          <Route
+            path='items/:itemId'
+            element={<ItemDetail />}
+          ></Route>
+          <Route
+            path='items/new'
+            element={<ItemNew />}
+          ></Route>
+          <Route
+            path='/categories/:categoryId'
             element={<Category images={state.images} />}
           ></Route>
         </Routes>
