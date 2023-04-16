ddec835973ff87120c45b0a81c06b86b56218d76
Jenny Carroll <jennygc@telus.net>
2023-04-16 17:59:00 -0600

    create countdown in ItemDetail

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,48 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Import components:
-import Items from './Items';
-import Nav from './Nav';
-import ItemDetail from './ItemDetail';
-import ItemEdit from './ItemEdit';
-import Category from './Category';
+import Items from "./Items";
+import Nav from "./Nav";
+import ItemDetail from "./ItemDetail";
+import ItemEdit from "./ItemEdit";
+import Category from "./Category";
 
 // Import hooks and helpers:
-import useApplicationData from '../hooks/useApplicationData';
+import useApplicationData from "../hooks/useApplicationData";
 
 // Import styling:
-import './App.scss';
+import "./App.scss";
 
 // MAIN FUNCTION
 export default function App() {
   // State management and functions:
   const { state, setState } = useApplicationData();
-  console.log(state);
 
   return (
     <BrowserRouter>
       <main>
-        <Nav
-          items={state.items}
-          categories={state.categories}
-        />
-
+        <Nav items={state.items} categories={state.categories} />
         <Routes>
+          <Route path="/" element={<Items images={state.images} />}></Route>
+          <Route path="items/:itemId" element={<ItemDetail />}></Route>
+          <Route path="items/new" element={<ItemEdit item={false} />}></Route>
           <Route
-            path='/'
-            element={<Items images={state.images} />}
-          ></Route>
-          <Route
-            path='items/:itemId'
-            element={<ItemDetail />}
-          ></Route>
-          <Route
-            path='items/new'
-            element={<ItemEdit item={false} />}
-          ></Route>
-          <Route
-            path='/categories/:categoryId'
+            path="/categories/:categoryId"
             element={<Category images={state.images} />}
           ></Route>
         </Routes>
--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -7,6 +7,8 @@ import { useParams } from "react-router-dom";
 function ItemDetail() {
   const params = useParams();
   const [itemObj, setItemObj] = useState({});
+  const [countdown, setCountdown] = useState(null);
+  const [countdownInterval, setCountdownInterval] = useState(null);
 
   useEffect(() => {
     axios
@@ -19,14 +21,51 @@ function ItemDetail() {
         console.log(error.response.headers);
         console.log(error.response.data);
       });
+    // Get the countdown date from itemObj
+    const countdownDate = new Date(itemObj.end_date);
+
+    // Update the countdown timer every second
+    const intervalId = setInterval(() => {
+      updateCountdown();
+    }, 1000);
+
+    setCountdownInterval(intervalId);
+
+    function updateCountdown() {
+      const now = new Date();
+      const timeDifference = countdownDate.getTime() - now.getTime();
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      if (timeDifference < 0) {
+        clearInterval(countdownInterval);
+        setCountdown("Bids are closed!");
+      } else {
+        setCountdown(
+          `Only ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds left!`
+        );
+      }
+    }
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
   }, []);
+
   console.log(itemObj);
+
   const bidToDollars = function (value) {
     return (itemObj.bid_value / 100).toLocaleString("en-US", {
       style: "currency",
       currency: "USD",
     });
   };
+
   return (
     <div className="itemDetail">
       <h1>
@@ -43,6 +82,7 @@ function ItemDetail() {
       <span>{itemObj.description}</span>
       <span>Current Bid: {bidToDollars(itemObj.bid_value)}</span>
       <button>BID NOW!</button> <span>Condition: {itemObj.condition}</span>
+      <div className="countdown-timer">{countdown}</div>
     </div>
   );
 }
