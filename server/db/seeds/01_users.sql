c397380e439e936bd0a6183532dc78e28a56129d
Jason-Wall <jason.waldick@gmail.com>
2023-04-16 15:41:49 -0600

    fix db seeding syntax

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -2,8 +2,8 @@ INSERT INTO users (
   name, email
 )
 VALUES 
-('Jenny Carroll', 'jenny@gmail.com');
-('Tyler Chessa', 'tylerchessa@gmail.com');
-('Jason Waldick', 'WaldickJ@gmx.com');
-('Petar Smith', 'petarsmith@gmail.com');
+('Jenny Carroll', 'jenny@gmail.com'),
+('Tyler Chessa', 'tylerchessa@gmail.com'),
+('Jason Waldick', 'WaldickJ@gmx.com'),
+('Petar Smith', 'petarsmith@gmail.com'),
 ('Rebecca Wilson', 'rebeccawilson@gmail.com');
