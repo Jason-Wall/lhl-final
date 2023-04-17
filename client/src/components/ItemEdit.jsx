321fefef266371fb8f365723ae4b896197f89f8f
Jason-Wall <jason.waldick@gmail.com>
2023-04-17 09:34:20 -0600

    Implement basic bootstrap styling

--- client/src/components/ItemEdit.jsx
+++ client/src/components/ItemEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import axios from 'axios';
 import './ItemEdit.scss';
 
@@ -12,6 +12,8 @@ function ItemEdit(props) {
   const [minBid, setMinBid] = useState(props.item.minBid);
 
   // SUPPORTING FUNCTIONS:
+  //populates options from
+
   // Collects form data from state and submits an axios.post
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -36,77 +38,95 @@ function ItemEdit(props) {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='itemNew'
-      autoComplete='off'
-    >
-      <span className='strong'>List a new item:</span>
-      <br />
-      <div>
-        <label htmlFor='item-title'>Title:</label>
-        <input
-          type='text'
-          name='item-title'
-          value={title}
-          onChange={(event) => {
-            setTitle(event.target.value);
-          }}
-          placeholder='Item Title'
-        />
-      </div>
-      <div>
-        <label htmlFor='item-description'>Description:</label>
-        <textarea
-          name='item-description'
-          value={description}
-          placeholder='Item Description'
-          onChange={(event) => {
-            setDescription(event.target.value);
-          }}
-        ></textarea>
-      </div>
-      <div>
-        <label htmlFor='item-condition'>Condition:</label>
-        <input
-          type='number'
-          name='item-condition'
-          value={condition}
-          placeholder='Item Condition'
-          onChange={(event) => {
-            setCondition(event.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor='item-category'>Category:</label>
-        <input
-          type='number'
-          name='item-category'
-          value={category}
-          placeholder='Item Category'
-          onChange={(event) => {
-            setCategory(event.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor='item-auction'>Auction End:</label>
-        <input
-          type='datetime-local'
-          name='item-auction'
-          value={endDate}
-          placeholder='Auction End'
-          onChange={(event) => {
-            setEndDate(event.target.value);
-          }}
-        />
-      </div>
+    <div className='itemEdit container'>
+      <form
+        onSubmit={handleSubmit}
+        className='itemNew'
+        autoComplete='off'
+      >
+        <span className={'strong'}>List a new item:</span>
+        <br />
+        <div className={'form-group'}>
+          <label htmlFor='item-title'>Title:</label>
+          <input
+            className={'form-control'}
+            type='text'
+            name='item-title'
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            placeholder='Item Title'
+          />
+        </div>
+        <div className={'form-group'}>
+          <label htmlFor='item-description'>Description:</label>
+          <textarea
+            className={'form-control'}
+            name='item-description'
+            value={description}
+            placeholder='Item Description'
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          ></textarea>
+        </div>
+        <div className={'form-group'}>
+          <label htmlFor='item-condition'>Condition:</label>
+          <input
+            className={'form-control'}
+            type='number'
+            name='item-condition'
+            value={condition}
+            placeholder='Item Condition'
+            onChange={(event) => {
+              setCondition(event.target.value);
+            }}
+          />
+        </div>
+        <div className={'form-group'}>
+          <label htmlFor='item-category'>Category:</label>
+          <input
+            className={'form-control'}
+            type='number'
+            name='item-category'
+            value={category}
+            placeholder='Item Category'
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+          />
+        </div>
+        <div className={'form-group'}>
+          <label htmlFor='item-bid'>Minimum Bid:</label>
+          <input
+            className={'form-control'}
+            type='number'
+            name='item-bid'
+            value={minBid}
+            placeholder='Minimum Bid'
+            onChange={(event) => {
+              setMinBid(event.target.value);
+            }}
+          />
+        </div>
+        <div className={'form-group'}>
+          <label htmlFor='item-auction'>Auction End:</label>
+          <input
+            className={'form-control'}
+            type='datetime-local'
+            name='item-auction'
+            value={endDate}
+            placeholder='Auction End'
+            onChange={(event) => {
+              setEndDate(event.target.value);
+            }}
+          />
+        </div>
 
-      <div className='modal-buttons'>
-        <button>Create Item</button>
-      </div>
-    </form>
+        <button className={'btn btn-dark'}>Create Item</button>
+      </form>
+    </div>
   );
 }
 
