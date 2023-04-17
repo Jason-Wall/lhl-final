f6f50514ce64cd52b1dd6b358e8c727868fc638e
Jenny Carroll <jennygc@telus.net>
2023-04-16 18:32:34 -0600

    add countdown

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./ItemDetail.scss";
 import axios from "axios";
-
 import { useParams } from "react-router-dom";
 
 function ItemDetail() {
@@ -15,49 +14,44 @@ function ItemDetail() {
       .get(`/items/${params.itemId}`)
       .then((res) => {
         setItemObj(res.data[0]);
+        const countdownDate = new Date(res.data[0].end_date);
+        const intervalId = setInterval(() => {
+          const now = new Date();
+          const timeDifference = countdownDate.getTime() - now.getTime();
+          const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+          const hours = Math.floor(
+            (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          );
+          const minutes = Math.floor(
+            (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+          );
+          const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+          if (timeDifference < 0) {
+            clearInterval(intervalId);
+            setCountdown("Bids are closed!");
+          } else {
+            // Check if any value is NaN before displaying countdown
+            const countdownValues = [days, hours, minutes, seconds];
+            const countdownString = countdownValues.some(isNaN)
+              ? null
+              : `Only ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds left!`;
+            setCountdown(countdownString);
+          }
+        }, 1000);
+
+        setCountdownInterval(intervalId);
       })
       .catch((error) => {
         console.log(error.response.status);
         console.log(error.response.headers);
         console.log(error.response.data);
       });
-    // Get the countdown date from itemObj
-    const countdownDate = new Date(itemObj.end_date);
 
-    // Update the countdown timer every second
-    const intervalId = setInterval(() => {
-      updateCountdown();
-    }, 1000);
-
-    setCountdownInterval(intervalId);
-
-    function updateCountdown() {
-      const now = new Date();
-      const timeDifference = countdownDate.getTime() - now.getTime();
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor(
-        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-      if (timeDifference < 0) {
-        clearInterval(countdownInterval);
-        setCountdown("Bids are closed!");
-      } else {
-        setCountdown(
-          `Only ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds left!`
-        );
-      }
-    }
-
-    // Clear the interval when the component unmounts
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(countdownInterval);
+    };
   }, []);
-
-  console.log(itemObj);
 
   const bidToDollars = function (value) {
     return (itemObj.bid_value / 100).toLocaleString("en-US", {
