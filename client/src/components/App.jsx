ddb0212821c42f1710294aecd55c05137cc3a87f
Jenny Carroll <jennygc@telus.net>
2023-04-17 19:20:42 -0600

    style ItemDetail and in Items.jsx wrap each item in a div 'item container'

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Import components:
 import Items from "./Items";
@@ -8,13 +8,13 @@ import ItemDetail from "./ItemDetail";
 import ItemEdit from "./ItemEdit";
 import Category from "./Category";
 import MyBids from "./MyBids";
-import AllBids from "./AllBids"
+import AllBids from "./AllBids";
 
 // Import hooks and helpers:
-import useApplicationData from '../hooks/useApplicationData';
+import useApplicationData from "../hooks/useApplicationData";
 
 // Import styling:
-import './App.scss';
+import "./App.scss";
 
 // MAIN FUNCTION
 export default function App() {
@@ -23,21 +23,21 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <main>
-        <Nav items={state.items} categories={state.categories} />
+      <Nav items={state.items} categories={state.categories} />
+      <main className="main">
         <Routes>
-          <Route path='/' element={<Items images={state.images} />}></Route>
-          <Route path='items/:itemId' element={<ItemDetail />}></Route>
+          <Route path="/" element={<Items images={state.images} />}></Route>
+          <Route path="items/:itemId" element={<ItemDetail />}></Route>
           <Route
-            path='items/new'
+            path="items/new"
             element={<ItemEdit item={false} categories={state.categories} />}
           ></Route>
           <Route
-            path='/categories/:categoryId'
+            path="/categories/:categoryId"
             element={<Category images={state.images} />}
           ></Route>
           <Route path="/bids" element={<AllBids />}></Route>
-           <Route path="/bids/:userId" element={<MyBids />}></Route>
+          <Route path="/bids/:userId" element={<MyBids />}></Route>
         </Routes>
       </main>
     </BrowserRouter>
