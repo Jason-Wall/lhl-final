a820dd06896e4c444033380796cf75509f9ef1cb
Jenny Carroll <jennygc@telus.net>
2023-04-17 19:51:18 -0600

    create favicon

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,42 +1,50 @@
-import './Nav.scss';
-import Search from './Search';
-import { Link } from 'react-router-dom';
+import "./Nav.scss";
+import Search from "./Search";
+import { Link } from "react-router-dom";
 
 function Nav(props) {
-
-  let categories = props.categories
+  let categories = props.categories;
 
   return (
-    <div className='nav nav-bar'>
+    <div className="nav nav-bar">
       <div className="nav top-nav">
         <div className="nav logo btn mb1 bg-black">
-          <Link to={'/'}>
-          <h1>SFBS</h1></Link>
+          <Link to={"/"}>
+            <h1>SFBS</h1>
+          </Link>
         </div>
-        <div className="nav search"><Search items={props.items}/></div>
+        <div className="fav">
+          sell<br></br>buy
+        </div>
+        <div className="nav search">
+          <Search items={props.items} />
+        </div>
         <div className="nav right-nav">
-          <div className='btn mb1 bg-black'><Link to={'/items/new'}>
-          Sell Now</Link></div>  
-          <div className='btn mb1 bg-black dropdown'>
-                Profile
-               <div className="dropdown-content">
-               <Link to={`/items/${4}`}>My Items</Link>
-               <Link to={`/profile/${4}`}>My Profile</Link>
-               <Link to={`/logout`}>logout</Link>
-               </div>
+          <div className="btn mb1 bg-black">
+            <Link to={"/items/new"}>Sell Now</Link>
           </div>
-          <div className='btn mb1 bg-black'><Link to={`bids/${4}`}>My Bids</Link></div>
+          <div className="btn mb1 bg-black dropdown">
+            Profile
+            <div className="dropdown-content">
+              <Link to={`/items/${4}`}>My Items</Link>
+              <Link to={`/profile/${4}`}>My Profile</Link>
+              <Link to={`/logout`}>logout</Link>
+            </div>
+          </div>
+          <div className="btn mb1 bg-black">
+            <Link to={`bids/${4}`}>My Bids</Link>
+          </div>
+        </div>
       </div>
-      </div>
-      <div className="nav bottom-nav"> 
+      <div className="nav bottom-nav">
         <div className="nav categories">
-          {categories.map(category => (
-              <Link
+          {categories.map((category) => (
+            <Link
               key={category.id}
               to={`/categories/${category.id}`}
-              className='btn mb1 bg-black'
+              className="btn mb1 bg-black"
             >
-               <h2>{category.title}</h2>
+              <h2>{category.title}</h2>
             </Link>
           ))}
         </div>
